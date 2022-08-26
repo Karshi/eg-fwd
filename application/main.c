3f8ca9f1c04e026600f1ffb2a45c64cca8dd13e2
Karshi <karimsherif@rocketmail.com>
2022-08-26 20:43:30 +0200

    Updated tests for port and GPIO drivers

--- application/main.c
+++ application/main.c
@@ -1,11 +1,16 @@
 #include "intctrl.h"
 #include "port_driver.h"
-//#include "gpio.h"
-
+#include "gpio.h"
 
 Port_ConfigType portConfig[] =
 	{
-		{PA6, GPIO, LOW, OUTPUT, PULL_DOWN, STRENGTH_2MA}
+		{PA0,	GPIO,	LOW,	OUTPUT,	PULL_DOWN,	STRENGTH_2MA},
+		{PA1,	GPIO,	LOW,	OUTPUT,	PULL_DOWN,	STRENGTH_2MA},
+		{PA2,	GPIO,	LOW,	OUTPUT,	PULL_DOWN,	STRENGTH_2MA},
+		{PA3,	GPIO,	LOW,	OUTPUT,	PULL_DOWN,	STRENGTH_2MA},
+		{PA6,	GPIO,	LOW,	OUTPUT,	PULL_DOWN,	STRENGTH_2MA},
+		{PE2,	GPIO,	LOW,	OUTPUT,	OPEN_DRAIN,	STRENGTH_4MA},
+		{PF3,	GPIO,	LOW,	INPUT,	PULL_UP,		STRENGTH_8MA}
 	};
 		
 uint32_t Port_count = sizeof(portConfig)/sizeof(portConfig[0]);
@@ -18,9 +23,18 @@ int main(void)
 	
 	while(1)
 	{
-//		for(int i = 0; i < 1000000; i++)
-//		{
-//			GPIOA->DATA ^= 1UL<<TEST_PIN;
-//		}
+		Dio_PortLevelType portLevel;
+		Dio_LevelType returnLevel;
+		Dio_LevelType level;
+		
+		Dio_WriteChannel(PA1, HIGH);
+		
+		Dio_WritePort(0, 85);	//0b01010101
+		
+		level = Dio_ReadChannel(PA6);
+		
+		portLevel = Dio_ReadPort(0);
+		
+		returnLevel = Dio_FlipChannel(PA6);
 	}
 }
